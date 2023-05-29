b5f57fe5bc18c25eb1c296b137ddc56ce3b6d3eb
skaddd <mateo.lebrun@telecomnancy.net>
2023-05-29 17:12:10 +0200

    Creating data folders updated requirements

--- pod_retrieve.py
+++ pod_retrieve.py
@@ -1,12 +1,19 @@
 import os
 import pytube
+from pytube.exceptions import VideoUnavailable
+
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def extract_podcast(youtube_link: str, saving_path: str = "./data") -> None:
     pod = pytube.YouTube(youtube_link)
-    pod.streams.filter(only_audio=True)[0].download(saving_path)
+    try:
+        pod.streams.filter(only_audio=True)[0].download(saving_path)
 
-    return
+    except VideoUnavailable:
+        log.info(f"{youtube_link} is not video, check the link given")
 
 
 if __name__ == "__main__":
